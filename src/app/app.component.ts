25866f36d7755049f332e4a1dc9333cb605026b1
Getze1221 <alebk201@gmail.com>
2025-06-10 08:04:38 -0600

    feat: componente featured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CategoriesPageComponent } from './categories-page/categories-page.component';
+import { CheckoutPageComponent } from "./checkout-page/checkout-page.component";
+import { FeaturedCategoriesComponent } from "./featured-categories/featured-categories.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CategoriesPageComponent],
+  imports: [RouterOutlet, CategoriesPageComponent, CheckoutPageComponent, FeaturedCategoriesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
