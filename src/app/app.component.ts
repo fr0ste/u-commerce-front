06493dabf4ce4a3c0fedc634343d52dc0e6bc542
Getze1221 <alebk201@gmail.com>
2025-06-10 10:16:17 -0600

    feat: componente footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CategoriesPageComponent } from './categories-page/categories-page.component';
 import { CheckoutPageComponent } from "./checkout-page/checkout-page.component";
 import { FeaturedCategoriesComponent } from "./featured-categories/featured-categories.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CategoriesPageComponent, CheckoutPageComponent, FeaturedCategoriesComponent],
+  imports: [RouterOutlet, CategoriesPageComponent, CheckoutPageComponent, FeaturedCategoriesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
