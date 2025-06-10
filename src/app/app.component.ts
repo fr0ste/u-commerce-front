69cae46dd2a73f161484f8530f0eba22ad73125c
Getze1221 <alebk201@gmail.com>
2025-06-10 10:28:31 -0600

    feat: componente product grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { CategoriesPageComponent } from './categories-page/categories-page.compo
 import { CheckoutPageComponent } from "./checkout-page/checkout-page.component";
 import { FeaturedCategoriesComponent } from "./featured-categories/featured-categories.component";
 import { FooterComponent } from "./footer/footer.component";
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
+import { ProductGridComponent } from "./product-grid/product-grid.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CategoriesPageComponent, CheckoutPageComponent, FeaturedCategoriesComponent, FooterComponent],
+  imports: [RouterOutlet, CategoriesPageComponent, CheckoutPageComponent, FeaturedCategoriesComponent, FooterComponent, HeroSectionComponent, ProductGridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
